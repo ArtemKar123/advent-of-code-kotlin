674c3eaf4592b5a2e4d72642440d38a16d0063af
ArtemKar123 <wartemtem@yandex.ru>
2022-12-05 17:01:53 +0100

    day 5

---
+++ src/Day05-2.kt
@@ -0,0 +1,37 @@
+import java.io.File
+import java.util.Stack
+
+fun main() {
+    val (inp, moves) = File("input", "inp5-1.txt").readText().split("\n\n")
+    val inp_split = inp.split("\n")
+    val n = inp_split.last().trim().split(" ").last().toInt()
+    val stacks: MutableList<Stack<Char>> = mutableListOf()
+    repeat(n) {
+        stacks.add(Stack<Char>())
+    }
+
+    inp_split.asReversed().slice(1 until inp_split.size).forEach {
+        it.chunked(4).forEachIndexed { index, symbol ->
+            if (symbol.trim().isNotEmpty()) {
+                stacks[index].push(symbol.trim()[1])
+            }
+        }
+    }
+
+    moves.split("\n").forEach {
+        val splitted = it.split(" ")
+        val from = splitted[3].toInt() - 1
+        val to = splitted[5].toInt() - 1
+        val s = Stack<Char>()
+        repeat(splitted[1].toInt()) {
+            s.push(stacks[from].pop())
+        }
+        repeat(splitted[1].toInt()) {
+            stacks[to].push(s.pop())
+        }
+    }
+    var ans = stacks.fold("") { acc, it ->
+        acc + it.pop()
+    }
+    println(ans)
+}
