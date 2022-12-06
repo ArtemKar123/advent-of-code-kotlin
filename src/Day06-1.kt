ff7c04db2c63fab31611150639647a9276dcc6f9
ArtemKar123 <wartemtem@yandex.ru>
2022-12-06 13:28:06 +0100

    day 6

---
+++ src/Day06-1.kt
@@ -0,0 +1,10 @@
+fun main() {
+    val dist = 4
+    val inp = readInput("inp6-1").first()
+    for (i in 0 until inp.length - dist) {
+        if (inp.slice(i..i + dist - 1).toSet().size == dist) {
+            println(i + dist)
+            break
+        }
+    }
+}
